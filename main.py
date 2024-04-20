4975ff2322c87da16916b69ba2694960c8598368
balugaq <129668183+balugaq@users.noreply.github.com>
2024-04-20 19:00:52 +0800

    删除了 name 的检查

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import yaml
 
 from time import time
 
-_VERSION = '1.3 REALEASE'
+_VERSION = '1.4 SHOT'
 MAXINT = 2147483647
 recipe_types = set("ENHANCED_CRAFTING_TABLE, MAGIC_WORKBENCH, ARMOR_FORGE, COMPRESSOR, PRESSURE_CHAMBER, SMELTERY, ORE_CRUSHER, GRIND_STONE, ANCIENT_ALTAR, NULL, GEO_MINER".split(', '))
 BIOMES = set("BADLANDS  BAMBOO_JUNGLE  BASALT_DELTAS  BEACH  BIRCH_FOREST  CHERRY_GROVE  COLD_OCEAN  CRIMSON_FOREST  CUSTOM DARK_FOREST  DEEP_COLD_OCEAN  DEEP_DARK  DEEP_FROZEN_OCEAN  DEEP_LUKEWARM_OCEAN  DEEP_OCEAN  DESERT  DRIPSTONE_CAVES  END_BARRENS  END_HIGHLANDS  END_MIDLANDS  ERODED_BADLANDS  FLOWER_FOREST  FOREST  FROZEN_OCEAN  FROZEN_PEAKS  FROZEN_RIVER  GROVE  ICE_SPIKES  JAGGED_PEAKS  JUNGLE  LUKEWARM_OCEAN  LUSH_CAVES  MANGROVE_SWAMP  MEADOW  MUSHROOM_FIELDS  NETHER_WASTES  OCEAN  OLD_GROWTH_BIRCH_FOREST  OLD_GROWTH_PINE_TAIGA  OLD_GROWTH_SPRUCE_TAIGA  PLAINS  RIVER  SAVANNA  SAVANNA_PLATEAU  SMALL_END_ISLANDS  SNOWY_BEACH  SNOWY_PLAINS  SNOWY_SLOPES  SNOWY_TAIGA  SOUL_SAND_VALLEY  SPARSE_JUNGLE  STONY_PEAKS  STONY_SHORE  SUNFLOWER_PLAINS  SWAMP  TAIGA  THE_END  THE_VOID  WARM_OCEAN  WARPED_FOREST  WINDSWEPT_FOREST  WINDSWEPT_GRAVELLY_HILLS  WINDSWEPT_HILLS  WINDSWEPT_SAVANNA  WOODED_BADLANDS  OTHERS".split('  '))
@@ -1267,17 +1267,6 @@ def checkResearches(addon):
         loadReg(data, position)
         did = data['id']
         isInt(did, position+'的 id')
-        name = data.get('name', null)
-        if name == null:
-            report(position+'的 name')
-            error('不能缺少名字！')
-        else:
-            for s in name:
-                if s in 'abcdefghijklmnopqrstuvwxyz0123456789.-_':
-                    report(position)
-                    error('研究ID只能由 abcdefghijklmnopqrstuvwxyz0123456789.-_ 中的字符组成')
-                    break
-
         for item in data['items']:
             isSlimefun(item, position+'的 items')
 
